e97be5ecb04d2428b2af04794eacce41855725f2
sumonDev13 <sumonmondal1530765@gmail.com>
2023-10-28 21:23:05 +0600

    errorHandler

--- controller/contactController.js
+++ controller/contactController.js
@@ -5,13 +5,25 @@ export const getContacts = (req,res)=>{
 }
 
 export const createContact = (req,res)=>{
-    res.status(200).json({message: 'create contact'});
+
+    console.log('body:',req.body);
+
+    const {name,email,phone} = req.body;
+    try {
+        if(!name || !email || !phone){
+            res.status(404).json({message: 'all fields required'});
+        }
+        
+    } catch (error) {
+        
+    }
+    res.status(201).json({message: 'create contact'});
 }
 
 export const updateContact = (req,res)=>{
-    res.status(201).json({message: `update contact$${req.params.id}`});
+    res.status(200).json({message: `update contact ${req.params.id}`});
 }
 
 export const deleteContact = (req,res)=>{
-    res.status(201).json({message:`delete contact ${req.params.id}` });
+    res.status(200).json({message:`delete contact ${req.params.id}` });
 }
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/contactRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
+app.use(express.json());
+app.use(errorHandler);
 dotenv.config();
 
-const port=process.env.PORT;
+const port = process.env.PORT;
 
 app.use('/api/contacts',router)
 
